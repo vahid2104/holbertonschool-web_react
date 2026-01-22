ec831786ab1c49a8b431f41dbc9d509917560879
vahid2104 <140729262+vahid2104@users.noreply.github.com>
2026-01-22 20:55:20 +0100

    Update index.js

--- Webpack/task_0/src/index.js
+++ Webpack/task_0/src/index.js
@@ -1,8 +1,5 @@
 import $ from 'jquery';
 
-$(document).ready(function() {
-  // Add three paragraphs to the body
-  $('body').append('<p>Holberton Dashboard</p>');
-  $('body').append('<p>Dashboard data for the students</p>');
-  $('body').append('<p>Copyright - Holberton School</p>');
-});
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<p>Copyright - Holberton School</p>');
