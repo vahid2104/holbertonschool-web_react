03b895c9c01beb831cb5d04992b2127345d3cfa3
vahid2104 <140729262+vahid2104@users.noreply.github.com>
2026-01-24 18:51:32 +0100

    Update dashboard_main.js

--- Webpack/task_2/js/dashboard_main.js
+++ Webpack/task_2/js/dashboard_main.js
@@ -8,11 +8,12 @@ $('body').append('<button>Click here to get started</button>');
 $('body').append('<p id="count"></p>');
 $('body').append('<p>Copyright - Holberton School</p>');
 
-let conunt = 0;
+let count = 0;
 
 function updateCounter() {
   count++;
   $("#count").html(`${count} clicks on the button`);
 };
+
 
 $('button').on('click', _.debounce(updateCounter, 500));
